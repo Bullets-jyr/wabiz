15441569fb695001c19fc48df5b07f60415bff65
Bullets-jyr <jyr@bullets.co.kr>
2024-06-23 16:29:50 +0900

    feat: 마이페이지 화면 위젯 구현 - 실습 (2)
    Closes #WAB-14

--- lib/views/my/my_page.dart
+++ lib/views/my/my_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
+import 'package:wabiz/theme.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -10,6 +13,120 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('마이페이지'),
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w600,
+          fontSize: 20,
+          color: AppColors.wabizGray[900],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 470,
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 24,
+                        backgroundColor: AppColors.bg,
+                      ),
+                      const Gap(8),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            onTap: () {},
+                            child: const Row(
+                              children: [
+                                Text(
+                                  '로그인하기',
+                                ),
+                                Icon(
+                                  Icons.keyboard_arrow_right,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Gap(8),
+                          const Text(
+                            '로그인 후 다양한 프로젝트에 참여해보세요.',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 28,
+                        backgroundColor: AppColors.wabizGray[200],
+                        child: SvgPicture.asset(
+                          'assets/icons/featured_seasonal_and_gifts.svg',
+                          width: 28,
+                          height: 28,
+                          colorFilter: const ColorFilter.mode(
+                            Colors.white,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      const Gap(20),
+                      const Text(
+                        '새로운 도전을\n시작해보세요,',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 13,
+                        ),
+                      ),
+                      const Gap(20),
+                      const Text(
+                        '개인 후원부터 제품, 콘텐츠, 서비스 출시, 성장까지 함께할게요.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // TODO: 로그인 처리 확인
+                      // TODO: 프로젝트 추가 화면으로 이동
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: AppColors.primary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '프로젝트 만들기',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
