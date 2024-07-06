17510b16f06a116a3d236227fa7f8d182d7f8181
Bullets-jyr <jyr@bullets.co.kr>
2024-07-06 22:44:25 +0900

    feat: 로그인 화면 위젯 구현 - 실습 (2)
    Closes #WAB-22

--- lib/views/login/sign_in_page.dart
+++ lib/views/login/sign_in_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:wabiz/theme.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
@@ -8,10 +12,118 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailTextController = TextEditingController();
+  TextEditingController passwordTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    emailTextController.dispose();
+    passwordTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(
+              '로그인',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: AppColors.wabizGray[900],
+              ),
+            ),
+            const Gap(32),
+            Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    controller: emailTextController,
+                    decoration: const InputDecoration(
+                      hintText: '이메일 입력',
+                    ),
+                  ),
+                  const Gap(20),
+                  TextFormField(
+                    controller: passwordTextController,
+                    decoration: const InputDecoration(
+                      hintText: '비밀번호 입력',
+                    ),
+                  ),
+                  const Gap(8),
+                  ButtonBar(
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          '로그인 정보를 잊으셨나요?',
+                          style: TextStyle(
+                            color: AppColors.wabizGray[600],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Gap(24),
+                  GestureDetector(
+                    onTap: () async {},
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: AppColors.secondary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          '이메일로 로그인하기',
+                          style: TextStyle(
+                            color: AppColors.primary,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Gap(24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '아직 계정이 없나요?',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.wabizGray[900],
+                        ),
+                      ),
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: AppColors.primary,
+                          textStyle: const TextStyle(
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.w500,
+                          )
+                        ),
+                        onPressed: () {
+                          context.push('/sign-up');
+                        },
+                        child: const Text('회원가입'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
