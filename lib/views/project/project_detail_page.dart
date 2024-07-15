203d4bc8dba1926e197282aec26bd1e05dc54f05
Bullets-jyr <jyr@bullets.co.kr>
2024-07-15 20:40:12 +0900

    feat: 프로젝트 상세 화면 위젯 구현 - 실습 (2)
    Closes #WAB-26

--- lib/views/project/project_detail_page.dart
+++ lib/views/project/project_detail_page.dart
@@ -1,4 +1,13 @@
+import 'dart:convert';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intl/intl.dart';
+import 'package:wabiz/model/project/project_model.dart';
+import 'package:wabiz/theme.dart';
 
 class ProjectDetailPage extends StatefulWidget {
   final String project;
@@ -13,14 +22,309 @@ class ProjectDetailPage extends StatefulWidget {
 }
 
 class _ProjectDetailPageState extends State<ProjectDetailPage> {
+  late ProjectItemModel projectItemModel;
+  bool isMore = false;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    print(widget.project);
+    projectItemModel = ProjectItemModel.fromJson(
+      json.decode(widget.project),
+    );
+    print(projectItemModel);
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              context.go('/home');
+            },
+            icon: SvgPicture.asset(
+              'assets/icons/home_outlined.svg',
+              width: 24,
+              height: 24,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 240,
+            decoration: BoxDecoration(
+              color: Colors.grey[300]!,
+              image: DecorationImage(
+                image: CachedNetworkImageProvider(
+                  projectItemModel.thumbnail ?? '',
+                ),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(.2),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(
+              16,
+              16,
+              16,
+              0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(projectItemModel.type ?? '??'),
+                    const Icon(
+                      Icons.keyboard_arrow_right,
+                    ),
+                  ],
+                ),
+                const Gap(12),
+                Text(
+                  '${projectItemModel.title}',
+                  style: const TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                const Gap(12),
+                Row(
+                  children: [
+                    Text(
+                      '${NumberFormat('###,###,###').format(
+                        (((projectItemModel.totalFunded ?? 0) /
+                                (projectItemModel.price ?? 1)) *
+                            100),
+                      )} % 달성',
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w800,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                    const Gap(12),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.secondary,
+                        borderRadius: BorderRadius.circular(3),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 6, vertical: 4),
+                      child: Text(
+                        '${DateTime.parse(projectItemModel.deadline ?? '').difference(DateTime.now()).inDays}일 남음',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const Gap(12),
+                Row(
+                  children: [
+                    Text(
+                      '${NumberFormat.currency(
+                        locale: "ko_KR",
+                        symbol: '',
+                      ).format(projectItemModel.totalFunded)}원 달성',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const Gap(12),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: AppColors.bg,
+                        borderRadius: BorderRadius.circular(3),
+                      ),
+                      padding: const EdgeInsets.symmetric(horizontal: 6),
+                      child: Text(
+                        '${projectItemModel.totalFundedCount} 명 참여',
+                        style: const TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            color: AppColors.newBg,
+            height: 32,
+          ),
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: SingleChildScrollView(
+                    physics: !isMore
+                        ? NeverScrollableScrollPhysics()
+                        : BouncingScrollPhysics(),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '프로젝트 스토리',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Gap(12),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.newBg,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: EdgeInsets.all(12),
+                            child: Text(
+                              '도서산간에 해당하는 서포터님은 배송 가능 여부를 반드시 메이커에게 문의 후 참여해주세요.',
+                            ),
+                          ),
+                          Gap(12),
+                          Image.asset(
+                            width: double.infinity,
+                            'assets/image/advertising_image.png',
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                if (!isMore)
+                  Positioned(
+                    left: 16,
+                    right: 16,
+                    bottom: 0,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [
+                              Colors.white,
+                              Colors.white,
+                              Colors.white,
+                              Colors.white,
+                              Colors.white.withOpacity(.1),
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter),
+                      ),
+                    ),
+                  ),
+                if (!isMore)
+                  Positioned(
+                    left: 16,
+                    right: 16,
+                    bottom: 16,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMore = true;
+                        });
+                      },
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                            color: AppColors.primary,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '스토리 더보기',
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: AppColors.primary,
+                              ),
+                            ),
+                            Gap(12),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              color: AppColors.primary,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 84,
+        color: Colors.white,
+        surfaceTintColor: Colors.white,
+        padding: EdgeInsets.zero,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                color: Colors.grey[200]!,
+              ),
+            ),
+          ),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 4),
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.favorite_border,
+                    ),
+                  ),
+                  const Text('1만+'),
+                ],
+              ),
+              const Gap(12),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 50,
+                  child: const Center(
+                    child: Text(
+                      '펀딩하기',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
